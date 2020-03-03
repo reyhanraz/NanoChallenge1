1709a780d74c921d1bd12332564cc8a133258796
Reyhan Rifqi <reyhanazzami@gmail.com>
2020-03-03 11:57:15 +0700

    Initial Commit

---
+++ NanoChallenge1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoChallenge1
+//
+//  Created by Reyhan Rifqi on 03/03/20.
+//  Copyright © 2020 Reyhan Rifqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
